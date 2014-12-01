4fdd1ebbc3b7b9b005c09ae377ab01c29654be04
sunny-g <sunny.gonna@gmail.com>
2014-12-01 15:20:39 -0800

    message signing works

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ if (lamport.verify(keys.pubKey, 'MESSAGE TO SIGN', signature)) {
  */
 
 var HASH_FUNC = 'SHA-256';
+
 var MSG_TYPE = 'TEXT';
 var HASH_OUTPUT = 'HEX';
 
@@ -19,25 +20,34 @@ var hash = function(msg, msg_type, output_type) {
   msg_type = msg_type || MSG_TYPE;
   output_type = output_type || HASH_OUTPUT;
   var hashObj = new jsSHA(msg, msg_type);
-  return hashObj.getHash(HASH_FUNC, output_type);
+  return hashObj.getHash(HASH_FUNC, output_type).slice(0, 32);
 };
 
-var random32ByteString = function() {
-  return JSON.stringify(secureRandom.randomArray(32))
+var random32Bytes = function() {
+  return secureRandom.randomArray(32);
+};
+
+var char2Binary = function(char) {
+  var binary = char.charCodeAt(0).toString(2);
+  while (binary.length < 8) {
+    binary = '0' + binary;
+  }
+  return binary;
 };
 
 var lamport = {
 
   generate: function() {
-
     var priv = [];
     var pub = [];
 
     for (var i = 0; i < 256; i++) {
-      var num1 = random32ByteString();
-      var num2 = random32ByteString();
-      var hash1 = hash(num1);
-      var hash2 = hash(num2);
+      var num1 = random32Bytes();
+      var num2 = random32Bytes();
+      var hash1 = hash(JSON.stringify(num1));
+      var hash2 = hash(JSON.stringify(num2));
+      // var hash1 = hash(num1.toString());
+      // var hash2 = hash(num2.toString());
 
       priv.push([num1, num2]);
       pub.push([hash1, hash2]);
@@ -49,11 +59,28 @@ var lamport = {
     }
   },
 
-  sign: function() {
+  sign: function(privKey, msg) {
+    /*
+    for each bit in the msgHash,
+      push to signature either the 0th or 1st hash in each pair of the pubkey
 
+     */
+    var msgHash = hash(msg);
+    var hashArray = msgHash.split('');
+    var signature = [];
+
+    hashArray.forEach(function(letter, index) {
+      var binary = char2Binary(letter).split('');
+      binary.forEach(function(bit, bitIdx) {
+        // signature.push( pubKey[index + bitIdx][bit] );
+        signature.push(privKey[index + bitIdx][bit]);
+      })
+    });
+
+    return signature;
   },
 
-  verify: function() {
+  verify: function(pubKey, msg) {
 
   }
 
--- test/tests.js
+++ test/tests.js
@@ -5,11 +5,11 @@ describe('lamport signatures', function() {
   describe('keypair creation', function () {
     it('should return the same hash for a given message', function () {
       var msg = 'this is a message';
-      var output_hash = new jsSHA(msg, 'TEXT').getHash('SHA-256', 'HEX');
+      var output_hash = new jsSHA(msg, 'TEXT').getHash('SHA-256', 'HEX').slice(0, 32);
       expect(output_hash).to.equal(hash(msg));
     });
 
-    it('should return a correct keypair, each an array of 256 arrays', function() {
+    it('should return a correct keypair, each an array of 256 tuples', function() {
       var keypair = lamport.generate();
 
       expect(keypair.privKey.length).to.equal(256);
@@ -18,14 +18,20 @@ describe('lamport signatures', function() {
       expect(keypair.pubKey[0]).to.be.an('array');
 
       var i = Math.floor(Math.random() * 256);
-      expect(hash(keypair.privKey[i][1])).to.equal(keypair.pubKey[i][1]);
+      expect(hash(JSON.stringify(keypair.privKey[i][1]))).to.equal(keypair.pubKey[i][1]);
+      // expect(hash( keypair.privKey[i][1].toString() )).to.equal(keypair.pubKey[i][1]);
     });
 
   });
 
-  describe('', function () {
-    it('', function () {
+  describe('message signing and verification', function () {
+    it('should return an Array[256] with each elem in privKey', function () {
+      var keypair = lamport.generate();
+      var msg = 'this is my sample message';
+      var signature = lamport.sign(keypair.privKey, msg);
 
+      expect(signature.length).to.equal(256);
+      expect(keypair.privKey[1]).to.contain(signature[1]);
     })
   });
 
