31e2a22afda3b9c474ea1b7a1372fce657b3f6ad
sunny-g <sunny.gonna@gmail.com>
2014-12-01 13:24:42 -0800

    generating keys (albeit, very inefficiently)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 /*
 EXAMPLE USAGE:
-var lamport = require('lamport')
 var keys = lamport.generate();
 
-var signature = lamport.sign(keys.private, 'MESSAGE TO SIGN')
+var signature = lamport.sign(keys.privKey, 'MESSAGE TO SIGN')
 
-if (lamport.verify(keys.public, 'MESSAGE TO SIGN', signature)) {
+if (lamport.verify(keys.pubKey, 'MESSAGE TO SIGN', signature)) {
   console.log('Authentic.');
 } else {
   console.log('Not Authentic!');
@@ -16,17 +15,38 @@ var HASH_FUNC = 'SHA-256';
 var MSG_TYPE = 'TEXT';
 var HASH_OUTPUT = 'HEX';
 
-var hash = function(msg) {
-  MSG_TYPE = MSG_TYPE || 'TEXT';
-  HASH_OUTPUT = HASH_OUTPUT || 'HEX';
-  var hashObj = new jsSHA(msg, 'TEXT');
-  return hashObj.getHash(HASH_FUNC, HASH_OUTPUT);
+var hash = function(msg, msg_type, output_type) {
+  msg_type = msg_type || MSG_TYPE;
+  output_type = output_type || HASH_OUTPUT;
+  var hashObj = new jsSHA(msg, msg_type);
+  return hashObj.getHash(HASH_FUNC, output_type);
+};
+
+var random32ByteString = function() {
+  return JSON.stringify(secureRandom.randomArray(32))
 };
 
 var lamport = {
 
   generate: function() {
 
+    var priv = [];
+    var pub = [];
+
+    for (var i = 0; i < 256; i++) {
+      var num1 = random32ByteString();
+      var num2 = random32ByteString();
+      var hash1 = hash(num1);
+      var hash2 = hash(num2);
+
+      priv.push([num1, num2]);
+      pub.push([hash1, hash2]);
+    }
+
+    return {
+      privKey: priv,
+      pubKey: pub
+    }
   },
 
   sign: function() {
--- test/tests.js
+++ test/tests.js
@@ -2,12 +2,25 @@ var expect = chai.expect;
 
 describe('lamport signatures', function() {
 
-  describe('hash creation', function () {
+  describe('keypair creation', function () {
     it('should return the same hash for a given message', function () {
       var msg = 'this is a message';
       var output_hash = new jsSHA(msg, 'TEXT').getHash('SHA-256', 'HEX');
       expect(output_hash).to.equal(hash(msg));
-    })
+    });
+
+    it('should return a correct keypair, each an array of 256 arrays', function() {
+      var keypair = lamport.generate();
+
+      expect(keypair.privKey.length).to.equal(256);
+      expect(keypair.pubKey.length).to.equal(256);
+      expect(keypair.privKey[0]).to.be.an('array');
+      expect(keypair.pubKey[0]).to.be.an('array');
+
+      var i = Math.floor(Math.random() * 256);
+      expect(hash(keypair.privKey[i][1])).to.equal(keypair.pubKey[i][1]);
+    });
+
   });
 
   describe('', function () {
